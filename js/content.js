51546b71c51637fde0e442dc786290f6d84f7fab
champion <jphilippe.champion@gmail.com>
2023-10-22 09:04:40 +0200

    mis en page des fiches technique

--- js/content.js
+++ js/content.js
@@ -36,15 +36,42 @@ $(document).ready(function(){
 
         $('#fiche-technique').click(function(){
         $('.annonces').hide();
+        $('.date-heure').hide();
     })
 
-        $('.annonces-peugeot').click(function(){
-
-            $('.annonces').show();
+        $('#annonces-peugeot').click(function(){
+            $('#description').hide();
+            $('.date-heure').hide();
         })
 
-
 });
+
+        $('#accueil').click(function(){
+            $('.annonces').hide();
+            $('#description').hide();
+
+        })
+
+var dateGlobal = new Date();
+var heureGlobal = new Date();
+
+var année = dateGlobal.getFullYear();
+var mois = dateGlobal.getMonth();
+var jour = dateGlobal.getDate();
+var heure = heureGlobal.getHours();
+var minutes = heureGlobal.getMinutes();
+var seconds = heureGlobal.getSeconds();
+var MOIS = [ "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
+ mois = MOIS[mois]
+
+document.getElementById("date").innerHTML = jour + " " + mois + " " + année ;
+document.getElementById("heure").innerHTML = heure + ":" + minutes + ":" + seconds;
+
+
+
+
+
+
 
 
 function loadDoc(){
