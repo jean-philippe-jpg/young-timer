d91644bb64b59cd6ea27cba85e68517b67f3c7d3
champion <jphilippe.champion@gmail.com>
2023-10-22 09:08:47 +0200

    transfert des données du dossier script vers content

--- js/script.js
+++ js/script.js
@@ -1,4 +1,51 @@
 
+$(document).ready(function(){
+
+   $('#106').click(function(){
+
+    $('.deux, .trois, .quatre, .cinq').hide();
+    $('.un').show();
+   })
+
+    $('#205').click(function(){
+
+        $('.un, .trois, .quatre, .cinq').hide();
+        $('.deux').show();
+    })
+
+    $('#305').click(function(){
+
+        $('.un, .deux, .quatre, .cinq').hide();
+        $('.trois').show();
+       })
+
+       $('#405').click(function(){
+
+        $('.un, .deux, .trois, .cinq').hide();
+        $('.quatre').show();
+       })
+
+       $('#505').click(function(){
+
+        $('.un, .deux, .trois, .quatre,').hide();
+        $('.cinq').show();
+       });
+
+        $('#fiche-technique').click(function(){
+        $('.annonces').hide();
+        $('.date-heure').hide();
+    })
+
+        $('.annonces-peugeot').click(function(){
+            $('.date-heure').hide();
+        })
+
+});
+
+        $('.accueil').click(function(){
+            $('.annonces').hide();
+        })
+
 
 var dateGlobal = new Date();
 var heureGlobal = new Date();
@@ -16,12 +63,6 @@ document.getElementById("date").innerHTML = jour + " " + mois + " " + année ;
 document.getElementById("heure").innerHTML = heure + ":" + minutes + ":" + seconds;
 
 
-$(document).ready(function(){
-    
-    $('#accueil').click(function(){
-    $('.date-heure').hide();
-    })
-});
 
 
 
